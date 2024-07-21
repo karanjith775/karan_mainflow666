67e45255980e8bcde9480f9e23e9c45ddfedbe8e
karanjith775 <129059127+karanjith775@users.noreply.github.com>
2024-07-21 17:55:47 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,14 @@
+fruits=['orange','musk melon','apple','strawberry']
+print(fruits)
+
+#adding an element to the list
+fruits.append('grapes')
+print(fruits)
+
+#removing an element to the list
+fruits.remove('orange')
+print(fruits)
+
+#get the length of the list
+length_of_the_list=len(fruits)
+print("length of the list:",fruits)
